ac2e24ea1dad255b43641e3402811955d672130b
Ricke1 <dim.kyz.200@gmail.com>
2022-08-02 17:51:07 +0700

    Добавлены проверки для state и addition

--- work_start/req/base_request.py
+++ work_start/req/base_request.py
@@ -12,10 +12,22 @@ class BaseRequest:
         if args is None:
             args = {"x": 1, "y": 1}
 
-        if self.name == "STATE" and self.type == "GET":
-            r = requests.get(ApiLocators.STATE_API_URL)
-            print(r.json())
+        if self.name == "state" and self.type == "GET":
+            try:
+                r = requests.get(ApiLocators.STATE_API_URL)
+            except requests.exceptions.ConnectionError:
+                assert False, "Не удалось установить соединение, возможно приложение не запущено"
+            info = r.json()
+            if info['statusCode'] == 0 and "OК" in info['state']:
+                return True
 
-        elif self.name == "ADDITION" and self.type == "POST":
-            r = requests.get(ApiLocators.ADDITION_API_URL, json=args)
-            return
+        elif self.name == "addition" and self.type == "POST":
+            try:
+                r = requests.post(ApiLocators.ADDITION_API_URL, json=args)
+            except requests.exceptions.ConnectionError:
+                assert False, "Не удалось установить соединение, возможно приложение не запущено"
+            info = r.json()
+            if info['statusCode'] == 0 and info['result'] == (args['x']+args['y']):
+                return True
+        else:
+            assert False, "Неправильный формат запроса"
--- work_start/test_webcalculator.py
+++ work_start/test_webcalculator.py
@@ -3,5 +3,8 @@ from .req.base_request import BaseRequest
 
 
 def test_request_response_correctness():
-    state = BaseRequest("STATE", "GET")
-    assert state.make_request()
+    state = BaseRequest("state", "GET")
+    assert state.make_request(), "Формат ответа не согласуется с описанием API-функции state"
+
+    addition = BaseRequest("addition", "POST")
+    assert  addition.make_request(), "Формат ответа не согласуется с описанием API-функции addition"
