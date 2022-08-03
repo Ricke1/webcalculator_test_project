36cb3d7c7d429deeb85cb1ea7eac6bf3e230fe35
Ricke1 <dim.kyz.200@gmail.com>
2022-08-03 16:12:51 +0700

    Добавлены проверки на возможность выключения калькулятора и смены адреса, а также проверка на запуск калькулятора по умолчанию в первом тесте

--- work_start/req/base_request.py
+++ work_start/req/base_request.py
@@ -4,15 +4,15 @@ import requests
 
 class BaseRequest:
 
-    def __init__(self, name, type):
+    def __init__(self, name, typeof):
         self.name = name
-        self.type = type
+        self.typeof = typeof
 
     def make_request(self, args=None):
         if args is None:
             args = {"x": 1, "y": 1}
 
-        if self.name == "state" and self.type == "GET":
+        if self.name == "state" and self.typeof == "GET":
             try:
                 r = requests.get(ApiLocators.STATE_API_URL)
             except requests.exceptions.ConnectionError:
@@ -23,7 +23,7 @@ class BaseRequest:
             else:
                 return False
 
-        elif self.name == "addition" and self.type == "POST":
+        elif self.name == "addition" and self.typeof == "POST":
             try:
                 r = requests.post(ApiLocators.ADDITION_API_URL, json=args)
             except requests.exceptions.ConnectionError:
@@ -34,7 +34,7 @@ class BaseRequest:
             else:
                 return False
 
-        elif self.name == "multiplication" and self.type == "POST":
+        elif self.name == "multiplication" and self.typeof == "POST":
             try:
                 r = requests.post(ApiLocators.MULTIPLICATION_API_URL, json=args)
             except requests.exceptions.ConnectionError:
@@ -46,7 +46,7 @@ class BaseRequest:
             else:
                 return False
 
-        elif self.name == "division" and self.type == "POST":
+        elif self.name == "division" and self.typeof == "POST":
             try:
                 r = requests.post(ApiLocators.DIVISION_API_URL, json=args)
             except requests.exceptions.ConnectionError:
@@ -57,7 +57,7 @@ class BaseRequest:
             else:
                 return False
 
-        elif self.name == "remainder" and self.type == "POST":
+        elif self.name == "remainder" and self.typeof == "POST":
             try:
                 r = requests.post(ApiLocators.REMAINDER_API_URL, json=args)
             except requests.exceptions.ConnectionError:
@@ -71,13 +71,23 @@ class BaseRequest:
         else:
             assert False, "Неправильный формат запроса"
 
-    @staticmethod
-    def check_application_management_functionality(test_mode=1):
+    def check_application_management_functionality(self, test_mode=1):
         # Если test_mode = 1, то происходит проверка: Выключился ли калькулятор?
         if test_mode == 1:
             try:
-                r = requests.get(ApiLocators.STATE_API_URL)
+                requests.get(ApiLocators.STATE_API_URL)
             except requests.exceptions.ConnectionError:
-                assert False, "Не удалось установить соединение, возможно приложение не запущено"
+                return True
+        # Если test_mode = 1, то происходит проверка:
         elif test_mode == 2:
-            return False
+            try:
+                r = requests.get(f"http://{self.name}:{self.typeof}/api/state")
+                print("Соединение установлено успешно")
+            except requests.exceptions.ConnectionError:
+                assert False, "Не удалось установить соединение, возможно приложение не запущено или указан неверный " \
+                              "адресс "
+            info = r.json()
+            if info['statusCode'] == 0 and "OК" in info['state']:
+                return True
+            else:
+                return False
--- work_start/test_webcalculator.py
+++ work_start/test_webcalculator.py
@@ -2,47 +2,80 @@ import json
 import os
 import time
 
+import pytest
+
 from .req.base_request import BaseRequest
 
 
+# Проверка на корректность формата запроса/ответа для API-методов и значения по умолчанию
 def test_request_response_correctness():
-    os.system('work_start\webcalculator.exe start')
+    os.system('work_start\\webcalculator.exe stop')
+    os.system('work_start\\webcalculator.exe start')
 
+    # Проверка задачи state
     state = BaseRequest("state", "GET")
     assert state.make_request(), "Формат ответа не согласуется с описанием API-функции state"
 
+    # Проверка задачи addition
     addition = BaseRequest("addition", "POST")
     assert addition.make_request(), "Формат ответа не согласуется с описанием API-функции addition"
 
+    # Проверка задачи multiplication
     multiplication = BaseRequest("multiplication", "POST")
     assert multiplication.make_request(), "Формат ответа не согласуется с описанием API-функции multiplication"
 
+    # Проверка задачи division
     division = BaseRequest("division", "POST")
     assert division.make_request(), "Формат ответа не согласуется с описанием API-функции division"
 
+    # Проверка задачи remainder
     remainder = BaseRequest("remainder", "POST")
     assert remainder.make_request(), "Формат ответа не согласуется с описанием API-функции remainder"
 
 
+# Проверка на правильность вычисления результата при указании допустимых входных данных
 def test_calculations_correctness():
     args = {"x": -2147483648, "y": 2147483647}
 
+    # Проверка задачи addition: сложение x и y
     addition = BaseRequest("addition", "POST")
     assert addition.make_request(args), "Результат вычисления webcalculator неверный"
 
+    # Проверка задачи multiplication: умножение x и y
     multiplication = BaseRequest("multiplication", "POST")
     assert multiplication.make_request(args), "Результат вычисления webcalculator неверный"
 
+    # Проверка задачи division: деление на цело x на y
     division = BaseRequest("division", "POST")
     assert division.make_request(args), "Результат вычисления webcalculator неверный"
 
+    # Проверка задачи remainder: остаток от деления x на y
     remainder = BaseRequest("remainder", "POST")
     assert remainder.make_request(args), "Результат вычисления webcalculator неверный"
 
 
-def test_webcalculator():
+# Проверка: Работает ли калькулятор после его выключения через консоль?
+def test_webcalculator_on_or_off():
+    print("Выключение калькулятора")
 
-    os.system('work_start\webcalculator.exe stop')
+    # Остановка калькулятора
+    os.system('work_start\\webcalculator.exe stop')
 
+    # Тестовый запрос, который проверяет наличие ошибки ConnectionError
     state = BaseRequest("state", "GET")
     assert state.check_application_management_functionality(test_mode=1)
+    print("Вебкалькулятор успешно выключен, запрос не отправлен ")
+
+# Проверка возможности смены адреса хоста/порта
+def test_webcalculator_change_host_or_port_address():
+    IP = 'localhost'
+    PORT = '5413'
+
+    os.system(f'work_start\\webcalculator.exe start {IP} {PORT}')
+
+    # Тестовый запрос, в котором имя задачи и тип запроса изменены на новый адрес и порт, так как имя задачи
+    # и тип запроса предопределены заранее
+    state = BaseRequest(IP, PORT)
+    assert state.check_application_management_functionality(test_mode=2),  "Получен неверный ответ от сервера"
+
+    os.system('work_start\\webcalculator.exe stop')
